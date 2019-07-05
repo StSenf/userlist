4c46cd086c6b963e4f979f9791402b8d6b589061
Stephanie Senf <stephanie.senf@posteo.de>
2019-07-05 14:15:17 +0200

    add default routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import {RouterModule, Routes} from '@angular/router';
 import {UserListComponent} from './user-list/user-list.component';
 
 const routes: Routes = [
-  { path: "users", component: UserListComponent }
+  { path: "users", component: UserListComponent },
+  { path: "", redirectTo: "/users", pathMatch: 'full' },
+  { path: "**", redirectTo: "/users", pathMatch: 'full' },
 ];
 
 @NgModule({
