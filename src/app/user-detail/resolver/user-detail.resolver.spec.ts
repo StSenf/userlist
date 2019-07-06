3baff8e679ddf63f9794caddef018746551b5c28
Stephanie Senf <stephanie.senf@posteo.de>
2019-07-06 19:33:49 +0200

    provide unit test for user detail resolver

--- src/app/user-detail/resolver/user-detail.resolver.spec.ts
+++ src/app/user-detail/resolver/user-detail.resolver.spec.ts
@@ -1,9 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { UserDetailResolver } from './user-detail.service';
+import {UserDetailResolver} from './user-detail.resolver';
+import {UserService} from '../../shared/user.service';
 
 describe('UserDetailService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        UserDetailResolver,
+        UserService,
+      ]
+    });
+  });
 
   it('should be created', () => {
     const service: UserDetailResolver = TestBed.get(UserDetailResolver);
