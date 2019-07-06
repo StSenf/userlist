86fba147d3a31a1fd28ea3edcd9ed41d7ed5d530
Stephanie Senf <stephanie.senf@posteo.de>
2019-07-06 19:22:56 +0200

    provide unit test for user list resolver

--- src/app/user-list/resolver/user-list.resolver.spec.ts
+++ src/app/user-list/resolver/user-list.resolver.spec.ts
@@ -1,12 +1,72 @@
-import { TestBed } from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ActivatedRoute} from '@angular/router';
+import {Observable, of} from 'rxjs';
 
-import { UserListResolver } from './user-list.service';
+import {UserListResolver} from './user-list.resolver';
+import {UserService} from '../../shared/user.service';
+
+import Spy = jasmine.Spy;
+
 
 describe('UserListService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: UserListResolver = TestBed.get(UserListResolver);
-    expect(service).toBeTruthy();
+  const userMock = [
+    {
+      id: 1,
+      name: "Leane Miller",
+      username: "coolleane",
+      email: "e@e.de",
+      address: {},
+      phone: "000",
+      website: "www",
+      company: {},
+    }
+  ];
+
+  let resolver: UserListResolver;
+  let route: ActivatedRoute;
+  let service: UserService;
+  let getSpy: Spy;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        UserListResolver,
+        UserService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            userList: userMock,
+          },
+        }
+      ]
+    });
+
+    const injector = getTestBed();
+    resolver = injector.get(UserListResolver);
+    service  = injector.get(UserService);
+    route    = injector.get(ActivatedRoute);
+
+    getSpy = spyOn(service, "getUsers")
+      .and
+      .returnValue(of(userMock));
   });
+
+  it("should resolve a observable", () => {
+    expect(resolver.resolve() instanceof Observable).toBe(true);
+  });
+
+  it("should call service methode", () => {
+    resolver
+      .resolve().subscribe(() => {
+      expect(getSpy).toHaveBeenCalled();
+    });
+  });
+
 });
